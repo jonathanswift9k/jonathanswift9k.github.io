ba085c7945a7868f51de3711e3a60715b6f4ed9a
Jonathan Swift <jonathan.swift9k@gmail.com>
2022-06-22 18:25:25 -0400

    About Update

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,7 +5,7 @@ import "../css/about.css"
 export default function About() {
     return (
         <div className="about">
-            <p>i have a deep passion for programming, with my ultimate goal being to one day land a role as a DevOps engineer. gaining new knowledge and problem solving are the two things that bring me the most satisfaction in every aspect of my life. whenever i pick up something new, i only want to do it in the best way it can possibly be done. outside of writing code, i love creating on digital art - mainly utilizing photoshop and procreate - tattooing, yoga and calisthenics, magic the gathering, reading, and trying new vegan recipes!</p>
+            <p>i have a deep passion for programming, with my ultimate goal being to one day secure a role as a DevOps engineer. increasing my knowledge pool and solving problems are the two things that bring me the most satisfaction in every aspect of my life. whenever i pick up something new, i only want to do it in the best way it can possibly be done. outside of writing code, i love creating digital art - mainly utilizing photoshop and procreate - tattooing, yoga and calisthenics, magic the gathering, reading, and trying new vegan recipes!</p>
         </div>
     )
 }
