40074787123998ef278383fac39051e2e386ff37
Jonathan Swift <jonathan.swift9k@gmail.com>
2023-09-04 16:24:24 -0400

    Job history home page update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,7 +7,7 @@ export default function Home() {
     return (
             <div className="home">
                 <p className="animate__animated animate__fadeInDown homeBg">hello, my name is jonathan swift.</p>
-                <p className="animate__animated animate__fadeInDown animate__delay-2s homeBg">i am a project director at InMoment, with a certification in full stack web development from V School</p>
+                <p className="animate__animated animate__fadeInDown animate__delay-2s homeBg">i am a application specialist at InMoment, with a certification in full stack web development from V School</p>
             </div>
     )
 }
